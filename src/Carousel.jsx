b45df568946678d97086774b62272b48657ea476
Tibor <tibor736@hotmail.com>
2023-07-12 02:24:19 +0300

    02: autoplay added

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,23 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { shortList, list, longList } from "./data";
 import { FaQuoteRight } from "react-icons/fa";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousel = () => {
-	const [people, setPeople] = useState(list);
+	const [people, setPeople] = useState(longList);
 	const [currentPerson, setCurrentPerson] = useState(0);
+
 	const prevSlide = () => {
 		setCurrentPerson((oldPerson) => {
 			const result = (oldPerson - 1 + people.length) % people.length;
 			return result;
 		});
 	};
+
 	const nextSlide = () => {
 		setCurrentPerson((oldPerson) => {
 			const result = (oldPerson + 1) % people.length;
 			return result;
 		});
 	};
+
+	useEffect(() => {
+		let sliderId = setInterval(() => {
+			nextSlide();
+		}, 3000);
+		return () => {
+			clearInterval(sliderId);
+		};
+	}, [currentPerson]);
 
 	return (
 		<section className="slider-container">
