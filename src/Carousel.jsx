81480cb4e8fa6945ea869806809917010838bc3c
Tibor <tibor736@hotmail.com>
2023-07-12 02:18:25 +0300

    02: slider next/prev functionality added

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -4,16 +4,38 @@ import { FaQuoteRight } from "react-icons/fa";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousel = () => {
-	const [people, setPeople] = useState(shortList);
-	const prevSlide = () => {};
-	const nextSlide = () => {};
+	const [people, setPeople] = useState(list);
+	const [currentPerson, setCurrentPerson] = useState(0);
+	const prevSlide = () => {
+		setCurrentPerson((oldPerson) => {
+			const result = (oldPerson - 1 + people.length) % people.length;
+			return result;
+		});
+	};
+	const nextSlide = () => {
+		setCurrentPerson((oldPerson) => {
+			const result = (oldPerson + 1) % people.length;
+			return result;
+		});
+	};
 
 	return (
 		<section className="slider-container">
-			{people.map((person) => {
+			{people.map((person, personIndex) => {
 				const { id, image, name, title, quote } = person;
 				return (
-					<article className="slide" key={id}>
+					<article
+						className="slide"
+						style={{
+							transform: `translateX(${
+								100 * (personIndex - currentPerson)
+							}%)`,
+							opacity: personIndex === currentPerson ? 1 : 0,
+							visibility:
+								personIndex === currentPerson ? "visible" : "hidden",
+						}}
+						key={id}
+					>
 						<img src={image} alt={name} className="person-img" />
 						<h5 className="name">{name}</h5>
 						<p className="title">{title}</p>
@@ -22,12 +44,12 @@ const Carousel = () => {
 					</article>
 				);
 			})}
-         <button type="button" className="prev" onClick={prevSlide}>
-            <FiChevronLeft />
-         </button>
-         <button type="button" className="next" onClick={nextSlide}>
-            <FiChevronRight />
-         </button>
+			<button type="button" className="prev" onClick={prevSlide}>
+				<FiChevronLeft />
+			</button>
+			<button type="button" className="next" onClick={nextSlide}>
+				<FiChevronRight />
+			</button>
 		</section>
 	);
 };
